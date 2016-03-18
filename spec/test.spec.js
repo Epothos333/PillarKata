ad5d75a32466b2b95dca8eac6da6e2525d9d793b
Edwin A <ea976@nyu.edu>
2016-03-18 13:02:21 -0400

    First Test - failed

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,7 +1,13 @@
-describe('Babysitting Kata', function(){
-  it('should be true', function() {
-    expect(calculatePay(true)).toBe(true);
+describe('jasmine test', function(){
+  it('true should be true', function() {
+    expect(true).toBe(true);
   });
 
 });
 
+describe('Babysitter recieves 12$/hr between start time and bedtime', function(){
+  it('if babysitter works from 5PM to 6PM, babysitter makes 12$', function() {
+    expect(calculatePay('5:00PM, 6:00PM')).toBe('$12.00');
+  });
+
+});
