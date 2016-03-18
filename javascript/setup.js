0a1a448aecbacbf34780f6f8d7dad4c7125f5613
Edwin A <ea976@nyu.edu>
2016-03-18 13:07:50 -0400

    Added code to pass test 3

--- javascript/setup.js
+++ javascript/setup.js
@@ -1,3 +1,8 @@
-function calculatePay(){
-  return '$12.00';
+function calculatePay(startTime, endTime){
+	var pay;
+	startTime = startTime.split("")[0];
+	endTime = endTime.split("")[0];
+	pay = (endTime - startTime)*12
+
+  return '$' + pay + '.00';
 }
--- spec/test.spec.js
+++ spec/test.spec.js
@@ -7,10 +7,10 @@ describe('jasmine test', function(){
 
 describe('Babysitter recieves 12$/hr between start time and bedtime', function(){
   it('if babysitter works from 5PM to 6PM, babysitter makes 12$', function() {
-    expect(calculatePay('5:00PM, 6:00PM')).toBe('$12.00');
+    expect(calculatePay('5:00PM', '6:00PM')).toBe('$12.00');
   });
   it('if babysitter works from 5PM to 7PM, babysitter makes 24$', function() {
-    expect(calculatePay('5:00PM, 7:00PM')).toBe('$24.00');
+    expect(calculatePay('5:00PM', '7:00PM')).toBe('$24.00');
   });
 
 });
