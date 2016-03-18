658774cd131bc62129e0af91a5de6d1f5b1f1595
Edwin A <ea976@nyu.edu>
2016-03-18 13:11:23 -0400

    Added failing test for bedtime to midnight pay

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -12,5 +12,14 @@ describe('Babysitter recieves 12$/hr between start time and bedtime', function()
   it('if babysitter works from 5PM to 7PM, babysitter makes 24$', function() {
     expect(calculatePay('5:00PM', '7:00PM')).toBe('$24.00');
   });
+  it('if babysitter works from 5PM to 8PM, babysitter makes 36$', function() {
+    expect(calculatePay('5:00PM', '8:00PM')).toBe('$36.00');
+  });
+});
+
+describe('Babysitter makes only $8/hr from bedtime (9PM) to midnight', function(){
+  it('if babysitter works from 9PM to 10PM, babysitter makes 8$', function() {
+    expect(calculatePay('9:00PM', '10:00PM')).toBe('$12.00');
+});
 
 });
