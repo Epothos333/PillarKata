b4126ee427c2e01b1c1508b9ac962e5aa0dc0d8a
Edwin A <ea976@nyu.edu>
2016-03-18 13:48:31 -0400

    refactored a lot since last test. All tests are passing now except for the latest one

--- javascript/setup.js
+++ javascript/setup.js
@@ -4,22 +4,15 @@ function calculatePay(startTime, endTime){
 	var j = 0;
 	var start = convertTime(startTime);
 	var end = convertTime(endTime);
-	if (/[AM]/.test(endTime) && end !== 12) {
+	if (/[A]/.test(endTime) && end !== 12) {
 		end +=12;
 	}
+	console.log(start);
+	console.log(end);
 
 
-	// while (end !== start) {
-	// 	if (end > 9 || end <= 12) {
-	// 		pay += 8;
-	// 	}
-	// 	else {
-	// 		pay += 12;
-	// 	}
-	// 	end -=1
-	//}
 	if (start >= 12) {
-		pay = (end - (start-12)) * 16;
+		pay = (end - start) * 16;
 	}
 	else if (start >= 9) {
 		pay = (end -start) * 8;
@@ -40,6 +33,6 @@ function calculatePay(startTime, endTime){
   		convertedTime += time[x];
   		x++
   	}
-  	return convertedTime;
+  	return Number(convertedTime);
   }
 }
